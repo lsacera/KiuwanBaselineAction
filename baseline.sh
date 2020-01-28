a15a900763541af11ef71aad363c0997f27b5b82
lsacera <57106550+lsacera@users.noreply.github.com>
2020-01-28 15:39:06 +0100

    Update baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -42,4 +42,12 @@ wget https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip
 # Unzip Kiuwan local analyzer
 unzip KiuwanLocalAnalyzer.zip -d $HOME/.
 # Execute Kiuwan Baseline
-$HOME/KiuwanLocalAnalyzer/bin/agent.sh --user $INPUT_USERID --pass $INPUT_PASSWORD --sourcePath $GITHUB_WORKSPACE --softwareName $INPUT_PROJECT --create --label $INPUT_LABEL
+#$HOME/KiuwanLocalAnalyzer/bin/agent.sh --user $INPUT_USERID --pass $INPUT_PASSWORD --sourcePath $GITHUB_WORKSPACE --softwareName $INPUT_PROJECT --create --label $INPUT_LABEL
+echo "Executing analyzer with"
+echo "--user " $INPUT_USERID 
+echo "--pass " $INPUT_PASSWORD
+echo "--sourcePath" $GITHUB_WORKSPACE 
+echo "--softwareName" $INPUT_PROJECT 
+echo "--create"
+echo "--label " $INPUT_LABEL
+echo "Executing analyzer with"
