7ff10c0415be1052a9ded2fbbf8c289fce1f5408
lsacera <57106550+lsacera@users.noreply.github.com>
2020-01-28 10:41:04 +0100

    Update baseline.sh
    some echoes...

--- baseline.sh
+++ baseline.sh
@@ -1,10 +1,22 @@
 #!/bin/sh -l
 
-echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
 
 #checking if java is installed in the image
+echo "Checking the JRE Version"
 java -version
 
+#checking some env variables passed to docker run
+echo $INPUT_PROJECT
+echo $INPUT_LABEL
+echo $INPUT_USERID
+echo $INPUT_PASSWORD
+echo $GITHUB_WORKFLOW
+echo $INPUT_REPOSITORY
 
+#checking the passed parameters 
+echo $1
+echo $2
+echo $3
+echo $4
