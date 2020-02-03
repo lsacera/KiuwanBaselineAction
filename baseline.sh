c66ecc5b9481d6bf24ea6fed3765e98d34550b6e
lsacera <57106550+lsacera@users.noreply.github.com>
2020-02-03 16:44:01 +0100

    Update baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 
-# Step 1: Download Kiuwan local analyzer
+# Download Kiuwan local analyzer
 wget https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip
-# Step 2: Unzip Kiuwan local analyzer
+# Unzip Kiuwan local analyzer
 unzip KiuwanLocalAnalyzer.zip -d $HOME/.
-# Step 3: Copy the configuration file to the kiuwan local analyzer folder
-cp /analyzer.properties $HOME/KiuwanLocalAnalyzer/conf/.
-# Step 4:Execute Kiuwan Baseline. 
+# Execute Kiuwan Baseline. 
 # TODO: remove the echoes in a later release
 echo "Executing analyzer with: "
 echo "--user " $INPUT_USERID 
