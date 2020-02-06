d935e5e49a773f4e201143af187b4f90c313fc9b
lsacera <57106550+lsacera@users.noreply.github.com>
2020-02-06 11:09:31 +0100

    Update baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -13,4 +13,4 @@ echo "--sourcePath" $GITHUB_WORKSPACE
 echo "--softwareName" $INPUT_PROJECT 
 echo "--create"
 echo "--label " $INPUT_LABEL
-$HOME/KiuwanLocalAnalyzer/bin/agent.sh --user $INPUT_USERID --pass $INPUT_PASSWORD --sourcePath \"$GITHUB_WORKSPACE\" --softwareName \"$INPUT_PROJECT\" --create --label \"$INPUT_LABEL\"
+/kla/KiuwanLocalAnalyzer/bin/agent.sh --user $INPUT_USERID --pass $INPUT_PASSWORD --sourcePath \"$GITHUB_WORKSPACE\" --softwareName \"$INPUT_PROJECT\" --create --label \"$INPUT_LABEL\"
