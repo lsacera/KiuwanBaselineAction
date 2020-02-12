606d77cc44f69bdfaafabb6fdb0da6dfb1d98acc
lsacera <57106550+lsacera@users.noreply.github.com>
2020-02-12 12:40:06 +0100

    Update kla.py

--- kla.py
+++ kla.py
@@ -11,46 +11,39 @@ import stat
 from pathlib import Path
 
 #print environment, the results should be there
-print("--------------------- ENVIRONMENT ------------------------")
-print (os.environ)
-print("--------------------- ENVIRONMENT ------------------------")
+#print("--------------------- ENVIRONMENT ------------------------")
+#print (os.environ)
+#print("--------------------- ENVIRONMENT ------------------------")
 
-#Params used in the call to the baseline analysis. TODO: check the parameters...
+#Params used in the call to the baseline analysis. 
+#All the parameters set in the action are stored as environment variables with INPUT_ prefix
 PARAM_KLA_USERNAME = os.environ['INPUT_USERID'] 
 PARAM_KLA_PASSWORD = os.environ['INPUT_PASSWORD'] 
 PARAM_KLA_APPNAME = os.environ['INPUT_PROJECT'] 
 PARAM_KLA_SOURCEDIR = os.environ['GITHUB_WORKSPACE'] 
 PARAM_KLA_MAXMEMORY = 'memory.max=' + os.environ['INPUT_MAXMEMORY'] + 'm' 
 PARAM_KLA_APPNAME = os.environ['INPUT_PROJECT']
-
-#Remaining params to define and check
 PARAM_KLA_INCLUDEPATTERNS = os.environ['INPUT_INCLUDEPATTERNS']
 PARAM_KLA_EXCLUDEPATTERNS = os.environ['INPUT_EXCLUDEPATTERNS']
 PARAM_KLA_TIMEOUT = os.environ['INPUT_TIMEOUT']
 PARAM_KLA_DATABASETYPE = os.environ['INPUT_DATABASETYPE']
 
-#Log parameters
-#print ('user:',PARAM_KLA_USERNAME, 'appname:',PARAM_KLA_APPNAME,'sourcedir:',PARAM_KLA_SOURCEDIR ,'maxmem:', PARAM_KLA_MAXMEMORY,'includes:',PARAM_KLA_INCLUDEPATTERNS,'excludes:',PARAM_KLA_EXCLUDEPATTERNS ,'timeout:',PARAM_KLA_TIMEOUT,'database:',PARAM_KLA_DATABASETYPE)
-
 KLA_URL = 'https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip'
 TMP_EXTRACTION_DIR = os.environ['WORKSPACE']  + '/kla'
 KLA_EXE_DIR = TMP_EXTRACTION_DIR + "/KiuwanLocalAnalyzer/bin"
 
-
+#Function to create the Kiuwan KLA line command.
+#Note the memory parameter has been already created properly
 def GetKLACmd( tmp_dir=TMP_EXTRACTION_DIR,
                appname=PARAM_KLA_APPNAME,
                sourcedir=PARAM_KLA_SOURCEDIR,
                user=PARAM_KLA_USERNAME,
                password=PARAM_KLA_PASSWORD,
-               mem='1024m'):
+               mem=PARAM_KLA_MAXMEMORY):
 
     prefix = tmp_dir + '/KiuwanLocalAnalyzer/bin/'
     agent = prefix + 'agent.sh'
     os.chmod(agent, stat.S_IRWXU)
-
-    #osgetcwd = r'%s' % os.getcwd().replace('\\','/')
-    #prefix = osgetcwd + '/' + temp + '/KiuwanLocalAnalyzer/bin'
-    #agent = prefix + '/' + agent
 
     klablcmd = '{} -c -n {} -s {} --user {} --pass {} {}'.format(agent,
                                                                  appname,
@@ -60,7 +53,8 @@ def GetKLACmd( tmp_dir=TMP_EXTRACTION_DIR,
                                                                  mem)
     return klablcmd
 
-
+#Function to download and extract the Kiuwan Local Analyzer from kiuwan server
+#TODO: parametrize the URL to support Kiuwan on premises installations
 def DownloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR, klaurl=KLA_URL ):
     print ('Downloading KLA zip from ', klaurl, ' at [', os.getcwd(), ']', '...')
     resp = urllib.request.urlopen(klaurl)
@@ -74,10 +68,11 @@ def DownloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR, klaurl=KLA_URL ):
       
     zipf.extractall(tmp_dir)
 
-
+#Parse the output of the analysis resutl to get the analysis code
 def GetBLAnalysisCodeFromKLAOutput( output ):
     return output.split("Analysis created in Kiuwan with code:",1)[1].split()[0]
 
+#Function to call the Kiuwan API to get the actual URL 
 def GetBLAnalysisResultsURL(analysis_code, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM_KLA_PASSWORD ): 
     apicall = "https://api.kiuwan.com/apps/analysis/" + analysis_code
     print ('Calling REST API [' , apicall , '] ...')
@@ -90,6 +85,7 @@ def GetBLAnalysisResultsURL(analysis_code, kla_user=PARAM_KLA_USERNAME, kla_pass
     #print ('URL del analisis: ' , jdata['analysisURL'])
     return jdata['analysisURL']
 
+#Function to excetute the actual Kiuwan Local Analyzer command line and get the resutls.
 def ExecuteKLA(cmd):
     print ('Executing [' , cmd , '] ...')
     pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -109,6 +105,7 @@ def ExecuteKLA(cmd):
     rc = pipe.wait()
     return output, rc
 
+#Actual executing code after defining the functions  
 # Extract and download KLA from kiuwan.com (or from on-premise site)
 DownloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR)
 
@@ -116,7 +113,6 @@ DownloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR)
 kla_bl_cmd = GetKLACmd(tmp_dir=TMP_EXTRACTION_DIR, mem=PARAM_KLA_MAXMEMORY)
 
 # Execute CLA KLI
-"""
 output, rc = ExecuteKLA(kla_bl_cmd)
 if rc == 0:
     print ('{}{}'.format('KLA return code: ', rc))
@@ -127,13 +123,7 @@ if rc == 0:
 else:
     print ('{}{}{}'.format('Analysis finished with error code [', rc, ']'))
 
-"""
-
-#testing, not invoking kiuwan, remove after using...
-analysis_code = 'A-7e4-17034858ff4'
-url_analysis = GetBLAnalysisResultsURL(analysis_code)
-
-#set results as environment variables.
+#set results as environment variables. Not used anymore
 #print("::set-env name=RESULT::{}".format(0))
 #print("::set-env name=ANALYSISURL::{}".format(url_analysis))
 
