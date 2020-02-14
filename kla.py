d6719d76e6d3f0d4450ff51bf63cca161f3682d2
lsacera <57106550+lsacera@users.noreply.github.com>
2020-02-14 10:21:19 +0100

    Update kla.py
    new parameters

--- kla.py
+++ kla.py
@@ -17,41 +17,34 @@ from pathlib import Path
 
 # Params used in the call to the baseline analysis.
 # All the parameters set in the action are stored as environment variables with INPUT_ prefix
+PARAM_KLA_BASEURL = os.environ['INPUT_KIUWANBASEURL']
 PARAM_KLA_USERNAME = os.environ['INPUT_USERID']
 PARAM_KLA_PASSWORD = os.environ['INPUT_PASSWORD']
 PARAM_KLA_APPNAME = os.environ['INPUT_PROJECT']
 PARAM_KLA_SOURCEDIR = os.environ['GITHUB_WORKSPACE']
-PARAM_KLA_MAXMEMORY = 'memory.max=' + os.environ['INPUT_MAXMEMORY'] + 'm'
-PARAM_KLA_INCLUDEPATTERNS = os.environ['INPUT_INCLUDEPATTERNS']
-PARAM_KLA_EXCLUDEPATTERNS = os.environ['INPUT_EXCLUDEPATTERNS']
-PARAM_KLA_TIMEOUT = os.environ['INPUT_TIMEOUT']
 PARAM_KLA_DATABASETYPE = os.environ['INPUT_DATABASETYPE']
+PARAM_KLA_ADVANCEDPARAMS = os.environ['INPUT_ADVANCEDPARAMS']
 
-KLA_URL = 'https://www.kiuwan.com/pub/analyzer/KiuwanLocalAnalyzer.zip'
+KLA_URL = PARAM_KLA_BASEURL + '/pub/analyzer/KiuwanLocalAnalyzer.zip'
 TMP_EXTRACTION_DIR = os.environ['WORKSPACE'] + '/kla'
 KLA_EXE_DIR = TMP_EXTRACTION_DIR + "/KiuwanLocalAnalyzer/bin"
 
-
 # Function to create the Kiuwan KLA line command.
+# It is created with the minimum amount of parameters. Then the advanced parameters are passed in, the User is responsible for a good format
 # Note the memory parameter has been already created properly
 def getKLACmd(tmp_dir=TMP_EXTRACTION_DIR,
               appname=PARAM_KLA_APPNAME,
               sourcedir=PARAM_KLA_SOURCEDIR,
               user=PARAM_KLA_USERNAME,
               password=PARAM_KLA_PASSWORD,
-              mem=PARAM_KLA_MAXMEMORY):
+              dbtype=PARAM_KLA_DATABASETYPE
+              advanced=PARAM_KLA_ADVANCEDPARAMS):
     prefix = tmp_dir + '/KiuwanLocalAnalyzer/bin/'
     agent = prefix + 'agent.sh'
     os.chmod(agent, stat.S_IRWXU)
 
-    klablcmd = '{} -c -n {} -s {} --user {} --pass {} {}'.format(agent,
-                                                                 appname,
-                                                                 sourcedir,
-                                                                 user,
-                                                                 password,
-                                                                 mem)
+    klablcmd = '{} -c -n {} -s {} --user {} --pass {} transactsql.parser.valid.list={} {}'.format(agent, appname, sourcedir, user, password, dbtype, advanced)
     return klablcmd
-
 
 # Function to download and extract the Kiuwan Local Analyzer from kiuwan server
 # TODO: parametrize the URL to support Kiuwan on premises installations
@@ -68,7 +61,6 @@ def downloadAndExtractKLA(tmp_dir=TMP_EXTRACTION_DIR, klaurl=KLA_URL):
 
     zipf.extractall(tmp_dir)
 
-
 # Parse the output of the analysis resutl to get the analysis code
 def getBLAnalysisCodeFromKLAOutput(output_to_parse):
     return output_to_parse.split("Analysis created in Kiuwan with code:", 1)[1].split()[0]
@@ -76,7 +68,11 @@ def getBLAnalysisCodeFromKLAOutput(output_to_parse):
 
 # Function to call the Kiuwan API to get the actual URL
 def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM_KLA_PASSWORD):
-    apicall = "https://api.kiuwan.com/apps/analysis/" + a_c
+    apicall = "https://api.kiuwan.com"
+    if not PARAM_KLA_BASEURL
+      apicall = PARAM_KLA_BASEURL 
+    
+    apicall = apicall + "/apps/analysis/" + a_c"
     print('Calling REST API [', apicall, '] ...')
 
     response = requests.get(apicall, auth=requests.auth.HTTPBasicAuth(kla_user, kla_password))
@@ -86,7 +82,6 @@ def getBLAnalysisResultsURL(a_c, kla_user=PARAM_KLA_USERNAME, kla_password=PARAM
     jdata = json.loads(response.content)
     # print ('URL del analisis: ' , jdata['analysisURL'])
     return jdata['analysisURL']
-
 
 # Function to excetute the actual Kiuwan Local Analyzer command line and get the resutls.
 def executeKLA(cmd):
@@ -107,7 +102,6 @@ def executeKLA(cmd):
         
     #return_code = pipe.wait()
     return output_text, pipe.returncode
-
 
 # Actual executing code after defining the functions
 # Extract and download KLA from kiuwan.com (or from on-premise site)
